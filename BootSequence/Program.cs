c0979d6a4d0c80bd05f2e585ea67cffa13332164
Julian <julianstone98@gmail.com>
2021-08-30 14:06:25 +0200

    First Commit

---
+++ BootSequence/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace BootSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boot();
+        }
+
+        static void Boot()
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\t\t\t\t\t\t Boot Sequence");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------\n");
+
+            Thread.Sleep(1000);
+            Console.WriteLine("\t\t\t\t System Boot...");
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Thread.Sleep(1000);
+            Console.WriteLine("Doing fancy computer stuff that you wouldn't understand.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Finishing the fancy computer stuff that a lower lifeform wouldn't understand.");
+            Thread.Sleep(3000);
+            Console.WriteLine("System Boot complete.");
+            Thread.Sleep(1000);
+
+            Console.Clear();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("\t\t\t\t\t\t Welcome to Stuff");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+
+        }
+    }
+}
